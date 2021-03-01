a7538ea357adfe5fca7dd0c962fe7f69476c6b78
charlottedrb <c.derbaghdassarian@gmail.com>
2021-03-01 17:31:06 +0100

    init project

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Comment;
+use App\Entity\Post;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Blog');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        return [
+            MenuItem::linktoDashboard('Dashboard', 'fa fa-home'),
+            // Users
+            MenuItem::section('Users'),
+            MenuItem::linkToCrud('Users', 'fa fa-users', User::class),
+
+            // Posts
+            MenuItem::section('Posts'),
+            MenuItem::linkToCrud('Posts', 'fa fa-file-alt', Post::class),
+
+            // Comments
+            MenuItem::section('Comments'),
+            MenuItem::linkToCrud('Comments', 'fa fa-comments', Comment::class),
+        ];
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+}
---
+++ src/Controller/Admin/PostCrudController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Post;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+
+class PostCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Post::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            AssociationField::new('author'),
+            TextEditorField::new('content'),
+            DateField::new('createdAt'),
+            BooleanField::new('isPublished'),
+            BooleanField::new('isDeleted'),
+        ];
+    }
+}

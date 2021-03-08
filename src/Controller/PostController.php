77a9abacfb07a3e8e87cf5c52b4cc70f58f1a5a0
Charlotte Der Baghdassarian <charlottederbaghdassarian@MacBook-Pro-de-Charlotte.local>
2021-03-08 16:09:07 +0100

    add post creation and layout of posts index

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Post;
+use App\Entity\User;
 use App\Form\CommentType;
 use App\Repository\CommentRepository;
 use App\Repository\PostRepository;
@@ -10,6 +12,7 @@ use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,11 +20,41 @@ use Symfony\Component\Routing\Annotation\Route;
 class PostController extends AbstractController
 {
     #[Route('/post', name: 'post')]
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
+        $posts = $postRepository->findAll();
+
+        $form = $this->createFormBuilder()
+            ->setAction($this->generateUrl('post_new'))
+            ->add('title', TextType::class)
+            ->add('content', TextareaType::class)
+            ->add('save', SubmitType::class)
+            ->getForm();
+
         return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
+            'posts' => $posts,
+            'form' => $form->createView()
         ]);
+    }
+
+    #[Route('/post/new', name: 'post_new')]
+    public function new(UserRepository $userRepository, Request $request){
+        $user = $userRepository->findOneBy(['username' => 'charlottedrb']);
+
+        if($request->isMethod('POST')) {
+            $data = $request->get('form');
+            $post = new Post();
+            $post->setAuthor($user);
+            $post->setTitle($data['title']);
+            $post->setContent($data['content']);
+            $post->setCreatedAt(new \DateTime('NOW'));
+            $post->setIsDeleted(false);
+            $post->setIsPublished(true);
+            $this->getDoctrine()->getManager()->persist($post);
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('post');
+        }
     }
 
     #[Route('/post/{id}', name: 'post_show')]
